273cc2c98f9cfd8275dda96ccfb2a8ee0b7de486
coderemon24 <mdemonahmed2021@gmail.com>
2025-05-24 11:36:55 +0600

    admin template mastering

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,6 @@
+@include('layouts.admin.head')
+@include('layouts.admin.sidebar-navbar')
+
+@yield('dashboard')
+
+@include('layouts.admin.footer')
---
+++ resources/views/layouts/admin/footer.blade.php
@@ -0,0 +1,29 @@
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <strong>Copyright &copy; <script>document.write(new Date().getFullYear());</script> <a href="https://kaizenitbd.com">Kaizen IT</a>.</strong>
+    All rights reserved.
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="{{asset('backend')}}/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="{{asset('backend')}}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE -->
+<script src="{{asset('backend')}}/dist/js/adminlte.js"></script>
+
+<!-- OPTIONAL SCRIPTS -->
+<script src="{{asset('backend')}}/plugins/chart.js/Chart.min.js"></script>
+<script src="{{asset('backend')}}/dist/js/pages/dashboard3.js"></script>
+</body>
+</html>
