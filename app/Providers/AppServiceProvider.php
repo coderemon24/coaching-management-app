68cf197744d1389d6dca83e36aeafe01cca6cea7
coderemon24 <mdemonahmed2021@gmail.com>
2025-05-27 14:25:46 +0600

    email setting changed via config from AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Repositories\Interfaces\EmailSettingRepositoryInterface;
 use App\Repositories\Interfaces\GeneralSettingRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,6 +22,22 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $general = app(GeneralSettingRepositoryInterface::class)->getFirst();
+        $email = app(EmailSettingRepositoryInterface::class)->getFirst();
+
+        if( $email) {
+            config([
+                'mail.mailers.smtp.transport' => $email->mail_driver,
+                'mail.mailers.smtp.host' => $email->mail_host,
+                'mail.mailers.smtp.port' => $email->mail_port,
+                'mail.mailers.smtp.username' => $email->mail_username,
+                'mail.mailers.smtp.password' => base64_decode($email->mail_password),
+                'mail.mailers.smtp.encryption' => $email->mail_encryption,
+                'mail.from.address' => $email->mail_from_address,
+                'mail.from.name' => $email->mail_from_name,
+            ]);
+        }
+
+        // Set general settings
         if ($general) {
             config([
                 'app.name' => $general->site_name,
